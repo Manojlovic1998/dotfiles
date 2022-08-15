7f6a330c10dc76e4861f56e8ed2da76f36ab94a1
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-15 08:17:18 +0200

    Add keymaps for neovim

---
+++ nvim/lua/maps.lua
@@ -0,0 +1,37 @@
+-- Basic Key Maps
+local keymap = vim.keymap
+
+
+-- Do not yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+-- Move window 
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', 's<left>', '<C-w>h')
+keymap.set('', 's<up>', '<C-w>k')
+keymap.set('', 's<down>', '<C-w>j')
+keymap.set('', 's<right>', '<C-w>l')
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+-- Resize Window Corne Keyboard QMK firmware NumPad Issue Walkaround
+keymap.set('n', '<C-j>', ':resize -2<CR>')
+keymap.set('n', '<C-k>', ':resize +2<CR>')
+keymap.set('n', '<C-l>', ':vertical :resize -2<CR>')
+keymap.set('n', '<C-h>', ':vertical :resize +2<CR>')
